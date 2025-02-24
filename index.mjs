b74a82ed78c258c952906c3969bbd7dcf42223b3
Emanuel Junior <156398698+OPifenomeno@users.noreply.github.com>
2025-02-24 01:56:26 -0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -13,6 +13,8 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
 let hisMessages = []
 
+
+
 if(fs.existsSync(historyPath)){
     const fileData = fs.readFileSync(historyPath,'utf-8')
     hisMessages = JSON.parse(fileData)
@@ -34,17 +36,17 @@ process.on('SIGINT',()=>{
 })
 function saveHistory(){
     let date = new Date()
-    localStorage.setItem("ultimaVez",date.toUTCString)
     fs.writeFileSync(historyPath,JSON.stringify(hisMessages,null,2),'utf-8')
 
 }
 
 app.post('/getResponse',async (req,res)  =>{
-    if (hisMessages.length == 0){
+    try{
+    if (hisMessages.length == 0 && !fs.existsSync(historyPath)){
         hisMessages.push({role:"system",content:process.env.PERSONALITY})
     }
     else{hisMessages.push({role:req.body.role,content:req.body.message})}
-
+    }catch(erro){console.log('DEU ERRO AQUI LINHA')}
     const client = new OpenAI({baseURL:process.env.ENDPOINT ,apiKey:process.env.API_KEY})
 
     const response = await client.chat.completions.create({
@@ -54,7 +56,7 @@ app.post('/getResponse',async (req,res)  =>{
         max_tokens:1000,
         model:"gpt-4o"
     })
- 
+
     const reply = response.choices[0].message.content
     hisMessages.push({role:"assistant",content:reply})
     res.json({result:reply})
